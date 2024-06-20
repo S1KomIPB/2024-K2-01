aadd0fd50a69cb9f129440fa6ad8a0b1d86df382
Adi Satria <adisatria@apps.ipb.ac.id>
2024-06-20 15:57:53 +0700

    refactor TopBar component and add dynamic title prop

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -1,19 +1,26 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
-import { Container } from "./_topBarStyle";
-import RightContent from "./RightContent/RightContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
+import UserMenu from "./RightContent/UserMenu";
+import { Container } from "./_topBarStyle";
 
-const TopBar: React.FC = () => {
-  const [user, loading, error] = useAuthState(auth)
+
+
+type TopBarProps = {
+  title: string | undefined
+};
+
+
+const TopBar: React.FC<TopBarProps> = ({title}) => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <Container>
-      <Flex justify='space-between' align='center'>
+      <Flex justify="space-between" align="center">
         <Text fontSize="16px" fontWeight="700" color="white">
-          Innovilage
+          {title}
         </Text>
-        <RightContent user={user} />
+        <UserMenu user={user} />
       </Flex>
     </Container>
   );
